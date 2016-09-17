43408b903d391c379eb94a3285e65546e023e72c
Vinh <vinhvien95@gmail.com>
2016-09-17 15:13:45 -0700

    Swords
    All assets in here

---
+++ Deflect/Assets/Script/MouseDrag.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour {
+
+    [SerializeField]
+    private float distance = 10f;
+    [SerializeField]
+    private float speed = .5f;
+    [SerializeField]
+    private float rotate = 10f;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+
+	void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 objPostition = Camera.main.ScreenToWorldPoint(mousePosition) + offset;
+
+        transform.position = objPostition;
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotate * Time.deltaTime, 0, 0);
+    }
+
+
+}
---
+++ Deflect/Assets/Script/Transparant.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparant : MonoBehaviour
+{
+    public GameObject[] swords;
+    public float startAlpha = 1.0f;
+    public float endAlpha = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        int swordCounter = 0;
+        while (swordCounter < swords.Length)
+        {
+            Renderer colorTarget = swords[swordCounter].GetComponentInChildren<Renderer>();
+            Color original = colorTarget.material.color;
+            original.a = Mathf.Lerp(startAlpha, endAlpha, (float)swordCounter / swords.Length);
+            colorTarget.material.color = original;
+            swordCounter++;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
