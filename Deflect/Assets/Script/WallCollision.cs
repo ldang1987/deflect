80e37fc6298aeeca746fb7998eac30c9f6db4fc2
jaydubyew <jwong206@gmail.com>
2016-09-18 14:05:47 -0700

    test

---
+++ Deflect/Assets/Script/WallCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollision : MonoBehaviour {
+
+	public Score_PTS scoreBoard; 
+	// private int count;
+	// Use this for initialization
+	void Start () {
+		// count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision otherObj) {
+		if (otherObj.gameObject.tag == "Projectile") {
+			// count += 1;
+			// Debug.Log ("Score " + count);
+			scoreBoard.increaseScoreCount();
+		}
+	}
+}
