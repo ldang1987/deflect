958d7d946cf210b6aaa6b28a26fc04e28621ff1e
Riley <rileyocall@gmail.com>
2016-09-18 00:39:43 -0700

    Added Mushrooms
    And sword scaling

---
+++ Deflect/Assets/bulletScaler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScaler : MonoBehaviour {
+
+    private float startTime;
+    private float extendTime;
+    private float narrowTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        extendTime = 1.0f;
+        narrowTime = 2.0f;
+
+        StartCoroutine(ScaleOverTime(2));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    /*if(Time.time < startTime + extendTime)
+        {
+            float extendLength = Mathf.Lerp(1, 500, Time.deltaTime / extendTime);
+            gameObject.transform.localScale.Set(gameObject.transform.localScale.x, extendLength, gameObject.transform.localScale.z);
+        } else if (Time.time < startTime + narrowTime) {
+            float scaleDown = Mathf.Lerp(1, 0, Time.deltaTime / extendTime);
+            gameObject.transform.localScale.Set(scaleDown, gameObject.transform.localScale.y, scaleDown);
+        }*/
+
+
+	}
+
+    IEnumerator ScaleOverTime(float time)
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 destinationScale = new Vector3(.01f, 500f, .01f);
+
+        float currentTime = 0.0f;
+        do
+        {
+            transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+
+            currentTime += Time.deltaTime;
+            yield return null;
+
+        } while (currentTime <= time);
+
+
+        Destroy(gameObject);
+    }
+}
---
+++ Deflect/Assets/swordScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swordScript : MonoBehaviour {
+
+    Rigidbody swordRigidbody;
+    Vector3 oldpos;
+    Vector3 newpos;
+    Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        swordRigidbody = gameObject.GetComponent<Rigidbody>();
+        oldpos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float flip = 1;
+        if(transform.rotation.eulerAngles.z < 180 && transform.rotation.eulerAngles.z > 0)
+        {
+            flip = -1;
+        }
+
+        newpos = transform.position;
+        var media = (newpos - oldpos);
+        velocity = media / Time.deltaTime;
+        oldpos = newpos;
+        newpos = transform.position;
+
+
+
+        Vector3 targetScale = new Vector3(.01f, flip * velocity.z * .015f + .01f, .01f);
+        Vector3 newScale = Vector3.Lerp(transform.localScale, targetScale, .5f);
+
+        transform.localScale = newScale;
+
+        Debug.Log(transform.rotation.eulerAngles);
+	}
+}
