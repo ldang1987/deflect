ea59eb5bc2e5fe63318d6f87be6cb0ac58b26d99
Riley <rileyocall@gmail.com>
2016-09-18 13:54:36 -0700

    Bullet Deflection to look target!

--- Deflect/Assets/FindLookTargetScript.cs
+++ Deflect/Assets/FindLookTargetScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FindLookTargetScript : MonoBehaviour {
 
+    [SerializeField] GameObject mainCamera;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,12 @@ public class FindLookTargetScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        Vector3 direction = mainCamera.gameObject.transform.forward; //rotation.eulerAngles;
+        //direction += new Vector3(0.0f, 90.0f, 0.0f);
+
+
+
+        Debug.DrawRay(mainCamera.gameObject.transform.position, direction);
 	}
 }
