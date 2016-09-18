3c72f38af82b6466b4a64aeb9c3c024509c5e71b
jaydubyew <jwong206@gmail.com>
2016-09-18 00:50:32 -0700

    my saves and all changes to the store assets

--- DeflectVR/Assets/Scripts/CannonBehavior.cs
+++ DeflectVR/Assets/Scripts/CannonBehavior.cs
@@ -17,14 +17,14 @@ public class CannonBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		/*if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			m_cannonRot.transform.Rotate(Vector3.up, -Time.deltaTime * 100f);
 		}
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
 			m_cannonRot.transform.Rotate(Vector3.up, Time.deltaTime * 100f);
-		} */
+		} 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
