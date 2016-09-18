4273c0689a1a7bb6b4080743c421305c3b95d18f
Riley <rileyocall@gmail.com>
2016-09-18 12:07:04 -0700

    Nine Point Complete

--- Deflect/Assets/Bullet.cs
+++ Deflect/Assets/Bullet.cs
@@ -6,17 +6,44 @@ public class Bullet : MonoBehaviour {
     [SerializeField] float bulletLife;
 
     private float dieTime;
+    public LayerMask swordLayer;
+    public Transform testTransform;
+    public float bulletSpeed;
+    private Rigidbody bulletRB;
 
 	// Use this for initialization
 	void Start () {
         dieTime = Time.time + bulletLife;
+        bulletRB = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         if(Time.time > dieTime)
         {
             Destroy(gameObject);
         }
     }
+
+    void OnCollisionEnter(Collider collision)    {
+        if(collision.GetComponent<Collider>().gameObject.layer == swordLayer)
+        {
+            Debug.Log("Hit Sword");
+            Transform bulletTarget = findDeflectTarget();
+
+            gameObject.transform.LookAt(bulletTarget);
+            bulletRB.velocity = transform.forward * bulletSpeed;
+        }
+    }
+
+    Transform findDeflectTarget()
+    {
+        //Transform output = new Transform();
+
+        Transform output = testTransform.transform;
+
+        return output;
+    }
+
 }
---
+++ Deflect/Assets/BulletDeflectTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDeflectTarget : MonoBehaviour {
+
+    public GameObject testTransform;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public Transform findDeflectTarget()
+    {
+        //Transform output = new Transform();
+
+        Transform output = testTransform.transform;
+
+        return output;
+    }
+}
